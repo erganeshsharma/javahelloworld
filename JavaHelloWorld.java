c38be49add02bc02c42dad2d27207227fd8e41b0
Ganesh Sharma <ganeshsharmait@gmail.com>
2019-02-27 13:12:59 +0530

    update JavaHelloWorld.java

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,10 +3,5 @@ public class JavaHelloWorld{
 	public static void main(String[] args){
 		System.out.println("Java Hello World");
 		System.out.println("Hello again");
-		System.out.println("Hello three times");
-		System.out.println("Hello four times");
-		System.out.println("Hello fifth times");
-		System.out.println("Hello sixth times");
-		System.out.println("Hello seven times");
 	}
 }
